e526b20e60ee1cbbc35b8a3f50d422822bf1e149
whynotcrybot <alexanderkatsero@gmail.com>
2018-03-21 15:32:26 +0200

    basic input pre-validation and sigint handling

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = require('./src/app.js')
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
-  terminal: false
+  output: process.stdout
 })
 
 const table = []
@@ -12,19 +11,31 @@ const players = []
 
 let firstln = true
 rl.on('line', line => {
-  const input = line.split(' ')
+  if (!line) rl.close()
+  const input = line.trim().split(' ')
 
   if (firstln) {
+    if (input.length !== 5) throw new Error('Table is supposed to have 5 cards')
+
     input.forEach(item => table.push(item))
     firstln = false
   } else {
+    if (input.length !== 3) throw new Error('Player is supposed to have 2 cards')
+
     players.push({
       name: input[0],
       cards: [input[1], input[2]]
     })
   }
+
+  rl.prompt()
+}).on('close', () => {
+  console.log(app(table, players))
+  process.exit(0)
+}).on('SIGINT', () => {
+  rl.close()
+  process.exit(0)
 })
 
-rl.on('close', () => {
-  console.log(app(table, players))
-})
+rl.setPrompt('')
+rl.prompt()
