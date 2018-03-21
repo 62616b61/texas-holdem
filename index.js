9ccae35547c45d6f2ac38c059a4c56d29aad57a7
whynotcrybot <alexanderkatsero@gmail.com>
2018-03-21 14:39:16 +0200

    load input from stdin

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+const readline = require('readline')
 const app = require('./src/app.js')
 
-const table = ['KS', 'AD', '3H', '7C', 'TD']
-const players = [
-  { name: 'John',  cards: ['9H', '7S'] },
-  { name: 'Becky', cards: ['JD', 'QC'] },
-  { name: 'Sam',   cards: ['AC', 'KH'] }
-]
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+})
 
-console.log( app(table, players) )
+const table = []
+const players = []
+
+let firstln = true
+rl.on('line', line => {
+  const input = line.split(' ')
+
+  if (firstln) {
+    input.forEach(item => table.push(item))
+    firstln = false
+  } else {
+    players.push({
+      name: input[0],
+      cards: [input[1], input[2]]
+    })
+  }
+})
+
+rl.on('close', () => {
+  console.log(app(table, players))
+})
