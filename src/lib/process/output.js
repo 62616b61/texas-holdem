7fcadd6890148e4a847f5942caba0be38a6b203e
whynotcrybot <alexanderkatsero@gmail.com>
2018-03-26 18:37:10 +0300

    group by rank AND combo cards

--- src/lib/process/output.js
+++ src/lib/process/output.js
@@ -26,7 +26,11 @@ module.exports = function output (array) {
    * It allows to determine effective kickers among each group.
    */
   const groups = array.reduce((acc, cur) => {
-    if (acc.length && acc[acc.length - 1][0].rule === cur.rule) {
+    if (
+      acc.length
+      && acc[acc.length - 1][0].rule === cur.rule
+      && cur.combo.every((card, i) => card === acc[acc.length - 1][0].combo[i])
+    ) {
       acc[acc.length - 1].push(cur)
     }
     else {
@@ -48,15 +52,12 @@ module.exports = function output (array) {
 
       /*
        * Kickers should be displayed only if the number of resulting lines
-       * is greater than 1 in a group and combo cards are different.
+       * is greater than 1 in a group.
        * Effective kickers are found by comparing kickers of
        * current resulting line and neighbouring resuling line (always next,
        * unless current resulting line is the last one in a group).
        */
-      const shouldDisplayKickers = group.length > 1
-        && combo.every((c, j) => group[last ? i - 1 : i + 1].combo[j] === c)
-
-      if (shouldDisplayKickers) {
+      if (group.length > 1) {
         line.push(separator)
         getEffectiveKickers(kickers, group[last ? i - 1 : i + 1].kickers)
           .map(face => parseInt(face) ? numToFace(face) : face)
